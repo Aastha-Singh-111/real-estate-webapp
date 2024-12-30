e0c6bff9d4f9e20f3ec3eb7f944dbc4983c4551b
Aastha-Singh-111 <aasthasinghclg4@gmail.com>
2024-12-30 16:16:20 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+   <>
+   <b className='text-red-900'>hello</b>
+   <h1 className='text-red-900 text-3xl'>App</h1>
+   <img src="https://th.bing.com/th/id/OIP.pcQSJmVjKOxWLu0nopQvtwHaE8?w=238&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7" alt="" />
+   </>
+  )
+}
